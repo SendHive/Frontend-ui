bd9dcb5ef9a41063a78898f0fd19fd7a22e1dbdf
vviveksharma <visharma@progress.com>
2025-03-30 04:23:12 +0530

    added thd job page
    Signed-off-by: vviveksharma <visharma@progress.com>

--- src/components/pages/job/job.jsx
+++ src/components/pages/job/job.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Job = () => {
-  return <div>Job</div>;
+  return (
+    <section className="min-h-screen flex flex-col justify-start pt-20 px-4 sm:px-10 bg-[#C8E6C9]">
+      <div className="mb-10 flex items-center justify-between">
+        <h1 className="font-medium text-4xl sm:text-xl md:text-2xl lg:text-3xl">
+          Mail Sender Service
+        </h1>
+        <button className="bg-[#4CAF50] hover:bg-[#81C784] text-white px-4 py-2 rounded-md ">
+          + Create
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Job;
